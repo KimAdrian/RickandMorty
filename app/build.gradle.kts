423e9da55614a351cc98717521a8332f2dfd271a
KimAdrian <adriankimutai5@gmail.com>
2022-11-17 19:29:31 +0300

    Setup dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id ("com.android.application")
     id ("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -37,11 +39,46 @@ android {
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.7.0")
+    implementation ("androidx.core:core-ktx:1.9.0")
     implementation ("androidx.appcompat:appcompat:1.5.1")
     implementation ("com.google.android.material:material:1.7.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.0")
+
+    //Glide
+    val glideVersion = "4.13.2"
+    implementation("com.github.bumptech.glide:glide:${glideVersion}")
+    kapt("com.github.bumptech.glide:compiler:${glideVersion}")
+
+    //Retrofit
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:${retrofitVersion}")
+    implementation("com.squareup.retrofit2:converter-moshi:${retrofitVersion}")
+
+    //Moshi
+    val moshiVersion = "1.13.0"
+    implementation("com.squareup.moshi:moshi-kotlin:${moshiVersion}")
+
+    //Navigation
+    val navigationVersion = "2.5.3"
+    implementation("androidx.navigation:navigation-fragment-ktx:${navigationVersion}")
+    implementation("androidx.navigation:navigation-ui-ktx:${navigationVersion}")
+
+    //Lifecycle-livedata
+    val lifecycleLivedataVersion = "2.5.1"
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${lifecycleLivedataVersion}")
+
+    //Hilt
+    val hiltVersion = "2.44"
+    implementation("com.google.dagger:hilt-android:${hiltVersion}")
+    kapt("com.google.dagger:hilt-compiler:${hiltVersion}")
+
 }
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
+
