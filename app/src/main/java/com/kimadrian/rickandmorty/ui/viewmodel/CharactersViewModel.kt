f2f596d40efe242a3b998027a32184ea9aaaa718
KimAdrian <adriankimutai5@gmail.com>
2022-12-27 00:33:50 +0300

    Nav Graph setup

--- app/src/main/java/com/kimadrian/rickandmorty/ui/viewmodel/CharactersViewModel.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/viewmodel/CharactersViewModel.kt
@@ -1,4 +1,8 @@
 package com.kimadrian.rickandmorty.ui.viewmodel
 
-class CharactersViewModel {
+import androidx.lifecycle.ViewModel
+import com.kimadrian.rickandmorty.data.repository.Repository
+
+class CharactersViewModel(private val characterRepository: Repository): ViewModel() {
+
 }
--- app/src/main/java/com/kimadrian/rickandmorty/ui/viewmodel/CharactersViewModelFactory.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/viewmodel/CharactersViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.kimadrian.rickandmorty.ui.viewmodel
 
-class CharactersViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.kimadrian.rickandmorty.data.repository.Repository
+
+@Suppress("unchecked_cast")
+class CharactersViewModelFactory(private val characterRepository: Repository): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(CharactersViewModel::class.java)) {
+            return CharactersViewModel(characterRepository) as T
+        }
+            throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
