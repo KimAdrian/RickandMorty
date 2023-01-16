ceba92a7ebd0a3e16dbef0ca19bcb18c6b775762
KimAdrian <adriankimutai5@gmail.com>
2023-01-16 22:20:31 +0300

    Finished repository layer implementation

--- app/src/main/java/com/kimadrian/rickandmorty/data/repository/Repository.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/data/repository/Repository.kt
@@ -1,4 +1,11 @@
 package com.kimadrian.rickandmorty.data.repository
 
+import com.kimadrian.rickandmorty.data.network.RetrofitInstance
+
 class Repository {
+    suspend fun getCharacters() = RetrofitInstance.apiService.getCharacter()
+
+    suspend fun getEpisodes() = RetrofitInstance.apiService.getEpisode()
+
+    suspend fun getLocations() = RetrofitInstance.apiService.getLocation()
 }
--- app/src/main/java/com/kimadrian/rickandmorty/ui/viewmodel/CharactersViewModel.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/viewmodel/CharactersViewModel.kt
@@ -1,8 +1,37 @@
 package com.kimadrian.rickandmorty.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.kimadrian.newsapp.utils.Resource
 import com.kimadrian.rickandmorty.data.repository.Repository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import timber.log.Timber
 
-class CharactersViewModel(private val characterRepository: Repository): ViewModel() {
+
+class CharactersViewModel(private val repository: Repository): ViewModel() {
+
+    fun getCharacters() = CoroutineScope(Dispatchers.IO).launch {
+        flow {
+            emit(Resource.loading(data = null))
+            try {
+                val characterResponse = repository.getCharacters()
+                if (characterResponse.isSuccessful && characterResponse.body() != null){
+                    emit(Resource.success(data = characterResponse.body()?.results))
+                }
+            } catch (e: HttpException) {
+                Timber.e("HttpException: ${e.message}")
+                emit(Resource.error(data = null, message = "HttpException: ${e.message}"))
+            } catch (e: ClassCastException) {
+                Timber.e( "ClassCastException: ${e.message}")
+            } catch (e: Exception) {
+                Timber.e( "Exception: ${e.message}")
+                emit(Resource.error(data = null, message = "Exception: ${e.message}"))
+            }
+
+        }
+    }
 
 }
