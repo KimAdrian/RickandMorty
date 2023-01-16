1d83a188ed7e510906025806fab52fe588ad926c
KimAdrian <adriankimutai5@gmail.com>
2023-01-17 02:53:50 +0300

    RecyclerAdapter logic implementation, added loading and error drawable files, refactord character card

--- app/src/main/java/com/kimadrian/rickandmorty/ui/adapter/CharactersRecyclerViewAdapter.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/adapter/CharactersRecyclerViewAdapter.kt
@@ -1,28 +1,48 @@
 package com.kimadrian.rickandmorty.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.kimadrian.rickandmorty.data.model.characters.Characters
+import com.bumptech.glide.Glide
+import com.kimadrian.rickandmorty.R
+import com.kimadrian.rickandmorty.data.model.characters.Result
+import com.kimadrian.rickandmorty.utils.CharacterStatus
 
-class CharactersRecyclerViewAdapter: ListAdapter<Characters, CharacterViewHolder>(DiffCallback){
+class CharactersRecyclerViewAdapter: ListAdapter<Result, CharacterViewHolder>(DiffCallback){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.character_card_item, parent, false)
+        return CharacterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val character = getItem(position)
+        holder.characterName.text = character.name
+        holder.characterStatus.text = character.status
+        holder.characterSpecies.text = character.species
+        Glide.with(holder.characterImage)
+            .load(character.image)
+            .placeholder(R.drawable.loading_animation)
+            .error(R.drawable.ic_broken_image)
+            .centerCrop()
+            .into(holder.characterImage)
+        holder.characterStatusColor.setCardBackgroundColor(CharacterStatus.statusColor(character.status))
+
     }
 
-    companion object DiffCallback: DiffUtil.ItemCallback<Characters>() {
-        override fun areItemsTheSame(oldItem: Characters, newItem: Characters): Boolean {
-            return oldItem.results == newItem.results
+    companion object DiffCallback: DiffUtil.ItemCallback<Result>() {
+        override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+            return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: Characters, newItem: Characters): Boolean {
+        override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
             return oldItem == newItem
         }
 
@@ -30,5 +50,9 @@ class CharactersRecyclerViewAdapter: ListAdapter<Characters, CharacterViewHolder
 }
 
 class CharacterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+    val characterImage: ImageView = itemView.findViewById(R.id.characterImage)
+    val characterName: TextView = itemView.findViewById(R.id.characterName)
+    val characterStatus: TextView = itemView.findViewById(R.id.characterStatus)
+    val characterSpecies: TextView = itemView.findViewById(R.id.characterSpecies)
+    val characterStatusColor: CardView = itemView.findViewById(R.id.statusColor)
 }
