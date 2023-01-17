e2057bd670d8d605e826d69934853aad17930bd0
KimAdrian <adriankimutai5@gmail.com>
2023-01-17 12:21:34 +0300

    Added null safety to info.next variable

--- app/src/main/java/com/kimadrian/rickandmorty/data/model/characters/Info.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/data/model/characters/Info.kt
@@ -2,7 +2,7 @@ package com.kimadrian.rickandmorty.data.model.characters
 
 data class Info(
     val count: Int,
-    val next: String,
+    val next: String?,
     val pages: Int,
     val prev: Any?
 )
