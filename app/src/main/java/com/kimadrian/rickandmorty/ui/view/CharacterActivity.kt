49b8be20de7588ae0ca0cbd7d0f1b1e372371cb4
KimAdrian <adriankimutai5@gmail.com>
2023-02-01 02:05:03 +0300

    Added loading and error composable to handle the different paging data loadStates

--- app/src/main/java/com/kimadrian/rickandmorty/ui/view/CharacterActivity.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/view/CharacterActivity.kt
@@ -3,14 +3,19 @@ package com.kimadrian.rickandmorty.ui.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Snackbar
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -20,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemsIndexed
@@ -35,6 +41,7 @@ import com.kimadrian.rickandmorty.ui.view.ui.theme.RickandMortyTheme
 import com.kimadrian.rickandmorty.ui.viewmodel.CharactersViewModel
 import com.kimadrian.rickandmorty.ui.viewmodel.CharactersViewModelFactory
 import com.kimadrian.rickandmorty.utils.CharacterStatusColor
+import com.valentinilk.shimmer.shimmer
 import kotlinx.coroutines.Dispatchers
 import timber.log.Timber
 
@@ -68,29 +75,35 @@ class CharacterActivity : ComponentActivity() {
 @Composable
 fun MyApp(characters: LazyPagingItems<Result>) {
 
-//    if (characters.loadState.refresh is LoadState.Loading) {
-//
-//    } else if {
-//        val error = when {
-//            characters.loadState.prepend is LoadState.Error -> characters.loadState.prepend as LoadState.Error
-//            characters.loadState.append is LoadState.Error -> characters.loadState.append as LoadState.Error
-//            characters.loadState.refresh is LoadState.Error -> characters.loadState.refresh as LoadState.Error
-//            else -> null
-//        }
-//        error?.let {
-//            Timber.e(it.error)
-//        }
-//    } else if {
-        LazyColumn {
-            itemsIndexed(characters) { _, character ->
-                character?.let {
-                    CharacterCard(character = it)
+    if (characters.loadState.refresh is LoadState.Loading) {
+        Column {
+            repeat(4){
+                LoadingShimmerCard()
+            }
+        }
+    }  else {
+        val error = when {
+            characters.loadState.prepend is LoadState.Error -> characters.loadState.prepend as LoadState.Error
+            characters.loadState.append is LoadState.Error -> characters.loadState.append as LoadState.Error
+            characters.loadState.refresh is LoadState.Error -> characters.loadState.refresh as LoadState.Error
+            else -> null
+        }
+        if (error != null){
+            error.let {
+                Timber.e(it.error)
+                ErrorScreen()
+            }
+        } else {
+            LazyColumn {
+                itemsIndexed(characters) { _, character ->
+                    character?.let {
+                        CharacterCard(character = it)
+                    }
                 }
             }
         }
-   // }
 
-
+    }
 
 }
 
@@ -149,7 +162,7 @@ fun CharacterCard(modifier: Modifier = Modifier, character: Result) {
                     Text(
                         text = character.status,
                         modifier = modifier
-                            .padding(top = 4.dp, start = 8.dp, end = 4.dp)
+                            .padding(top = 4.dp, start = 8.dp, end = 8.dp)
                             .wrapContentWidth()
                             .wrapContentHeight(),
                         fontSize = 18.sp,
@@ -167,7 +180,7 @@ fun CharacterCard(modifier: Modifier = Modifier, character: Result) {
                     Text(
                         text = character.species,
                         modifier = modifier
-                            .padding(top = 4.dp, start = 4.dp, end = 8.dp)
+                            .padding(top = 4.dp, start = 8.dp, end = 4.dp)
                             .wrapContentWidth()
                             .wrapContentHeight(),
                         color = Color.White,
@@ -203,10 +216,148 @@ fun CharacterCard(modifier: Modifier = Modifier, character: Result) {
 
 }
 
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.doofus_rick),
+            contentDescription = stringResource(id = R.string.error_image),
+            modifier = modifier
+                .size(350.dp)
+                .align(Alignment.Center),
+            contentScale = ContentScale.Fit
+        )
+        Snackbar(
+            modifier = modifier
+                .padding(10.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(text = "Something went wrong. Check your internet connection")
+        }
+
+
+    }
+
+
+}
+
+@Composable
+fun LoadingShimmerCard(modifier: Modifier = Modifier){
+    Card(
+        modifier = modifier
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = ColorCard
+    ) {
+        Row(
+            modifier = modifier
+                .height(intrinsicSize = IntrinsicSize.Min)
+                .fillMaxWidth()
+        ) {
+            // This represents the image
+            Spacer(
+                modifier = modifier
+                    .width(150.dp)
+                    .height(200.dp)
+                    .shimmer()
+                    .background(BackgroundColor)
+            )
+            Column(
+                modifier = modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+            ) {
+                //This represents the character name
+                Spacer(
+                    modifier = modifier
+                        .height(40.dp)
+                        .width(200.dp)
+                        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+                        .shimmer()
+                        .background(BackgroundColor)
+                )
+                Row(
+                    modifier = modifier
+                        .wrapContentHeight()
+                        .wrapContentWidth()
+                ) {
+                    //This represents the characterStatus card color
+                    Spacer(
+                        modifier = modifier
+                            .padding(top = 12.dp, start = 8.dp)
+                            .height(12.dp)
+                            .width(12.dp)
+                            .clip(RoundedCornerShape(10.dp))
+                            .shimmer()
+                            .background(BackgroundColor)
+                    )
+                    //This represents the characterStatus text
+                    Spacer(
+                        modifier = modifier
+                            .height(40.dp)
+                            .width(80.dp)
+                            .padding(top = 4.dp, start = 8.dp, end = 8.dp)
+                            .shimmer()
+                            .background(BackgroundColor)
+                    )
+                    //This represents the characterSpecies text
+                    Spacer(
+                        modifier = modifier
+                            .height(40.dp)
+                            .width(80.dp)
+                            .padding(top = 4.dp, start = 4.dp, end = 8.dp)
+                            .shimmer()
+                            .background(BackgroundColor)
+                    )
+                }
+                //This represents the last location label
+                Spacer(
+                    modifier = modifier
+                        .height(40.dp)
+                        .width(180.dp)
+                        .padding(top = 4.dp, start = 8.dp, end = 8.dp)
+                        .shimmer()
+                        .background(BackgroundColor)
+                )
+                //This represents the last location text
+                Spacer(
+                    modifier = modifier
+                        .height(40.dp)
+                        .width(200.dp)
+                        .padding(top = 4.dp, start = 8.dp, end = 8.dp)
+                        .shimmer()
+                        .background(BackgroundColor)
+                )
+
+            }
+
+
+        }
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RickandMortyTheme {
         //CharacterCard()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorPreview(){
+    ErrorScreen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShimmerPreview(){
+    LoadingShimmerCard()
 }
