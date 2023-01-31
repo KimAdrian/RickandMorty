fe0b8fa2a46b8f1dff55d1560fd6f6199e001b4e
KimAdrian <adriankimutai5@gmail.com>
2023-01-31 03:53:39 +0300

    Finished CharacterCard implementation

--- app/src/main/java/com/kimadrian/rickandmorty/ui/view/CharacterActivity.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/view/CharacterActivity.kt
@@ -3,14 +3,27 @@ package com.kimadrian.rickandmorty.ui.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.viewbinding.BuildConfig
+import com.kimadrian.rickandmorty.R
+import com.kimadrian.rickandmorty.ui.view.ui.theme.BackgroundColor
+import com.kimadrian.rickandmorty.ui.view.ui.theme.ColorCard
+import com.kimadrian.rickandmorty.ui.view.ui.theme.LabelTextColor
 import com.kimadrian.rickandmorty.ui.view.ui.theme.RickandMortyTheme
 import timber.log.Timber
 
@@ -25,9 +38,9 @@ class CharacterActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = BackgroundColor
                 ) {
-                    Greeting("Android")
+                    CharacterCard()
                 }
             }
         }
@@ -35,14 +48,125 @@ class CharacterActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CharacterCard(modifier: Modifier = Modifier, ) {
+    Card(
+        modifier = modifier
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = ColorCard
+    ) {
+        Row(
+            modifier = modifier
+                .height(intrinsicSize = IntrinsicSize.Min)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.rick_and_morty_icon),
+                contentDescription = stringResource(id = R.string.character_image),
+                modifier = modifier
+                    .width(150.dp)
+                    .height(200.dp)
+            )
+            Column(
+                modifier = modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.name),
+                    modifier = modifier
+                        .padding(top = 8.dp, start = 8.dp, end = 8.dp),
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Row(
+                    modifier = modifier
+                        .wrapContentHeight()
+                        .wrapContentWidth()
+                ) {
+                    Card(
+                        modifier = modifier
+                            .padding(start = 8.dp, top = 12.dp)
+                            .height(12.dp)
+                            .width(12.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        backgroundColor = Color.Green
+                    ){}
+                    Text(
+                        text = stringResource(id = R.string.alive),
+                        modifier = modifier
+                            .padding(top = 4.dp, start = 8.dp, end = 4.dp)
+                            .wrapContentWidth()
+                            .wrapContentHeight(),
+                        fontSize = 18.sp,
+                        color = Color.White
+                    )
+                    Text(
+                        text = stringResource(id = R.string.dash),
+                        modifier = modifier
+                            .padding(top = 4.dp)
+                            .wrapContentWidth()
+                            .wrapContentHeight(),
+                        color = Color.White,
+                        fontSize = 18.sp
+                    )
+                    Text(
+                        text = stringResource(id = R.string.species),
+                        modifier = modifier
+                            .padding(top = 4.dp, start = 4.dp, end = 8.dp)
+                            .wrapContentWidth()
+                            .wrapContentHeight(),
+                        color = Color.White,
+                        fontSize = 18.sp
+                    )
+                    
+                }
+                Text(
+                    text = stringResource(id = R.string.last_known_location),
+                    modifier = modifier
+                        .padding(top = 4.dp, start = 8.dp, end = 8.dp)
+                        .wrapContentHeight()
+                        .wrapContentWidth(),
+                    color = LabelTextColor,
+                    fontSize = 18.sp
+                )
+                Text(
+                    text = stringResource(id = R.string.last_location),
+                    modifier = modifier
+                        .padding(top = 4.dp, start = 8.dp, end = 8.dp)
+                        .wrapContentWidth()
+                        .wrapContentHeight(),
+                    color = Color.White,
+                    fontSize = 18.sp
+                )
+
+            }
+
+
+        }
+
+    }
+
+}
+
+@Composable
+fun Characters(){
+    LazyColumn {
+        item {
+            CharacterCard()
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RickandMortyTheme {
-        Greeting("Android")
+        CharacterCard()
     }
 }
--- app/src/main/java/com/kimadrian/rickandmorty/ui/view/ui/theme/Theme.kt
+++ app/src/main/java/com/kimadrian/rickandmorty/ui/view/ui/theme/Theme.kt
@@ -2,14 +2,16 @@ package com.kimadrian.rickandmorty.ui.view.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = ColorCard,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = BackgroundColor
 )
 
 private val LightColorPalette = lightColors(
@@ -29,11 +31,7 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun RickandMortyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+    DarkColorPalette
 
     MaterialTheme(
         colors = colors,
